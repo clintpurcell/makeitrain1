c02a77acd757fe8fbf2b792b083cd833ec5b07a0
clintpurcell@amdg.rockhursths.edu <clintpurcell@codelab-21.rockhurst.int>
2019-02-26 11:02:06 -0600

    git fix

--- Game-of-Circles---Python-master/GameOfCircles/Sprite.py
+++ Game-of-Circles---Python-master/GameOfCircles/Sprite.py
@@ -1,24 +1,26 @@
-class Jiggle:
-    
-    speed = 8
+class Sprite:
+    team = 2
     diameter = 50
-    c = color(0,0,255)
+    c = color(255)
     
     def __init__(self, x, y, team):
         self.x = x
         self.y = y
         self.team = team
         
-    def move(self):
-        self.y += random(-self.speed, self.speed)
-        self.x += random(-self.speed, self.speed)
-        self.x = constrain(self.x, 0, width)
-        self.y = constrain (self.y, 0, height)
+    def move():
+        pass
         
-    def display(self):
+    def displays(self):
         fill(self.c)
         ellipse(self.x, self.y, self.diameter, self.diameter)
         
     def animate(self):
         self.move()
         self.display()
+        
+    def isColliding(self, other):
+        r1 = self.diameter / 2.0
+        r2 = other.diameter / 2.0
+        return r1 + r2 > dist(self.x, self.y, other.x, other.y)
+    
